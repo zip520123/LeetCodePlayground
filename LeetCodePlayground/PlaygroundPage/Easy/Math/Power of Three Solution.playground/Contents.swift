fab07c078bbae4c17823decc81793bd4c307bfcc
zip520123 <zip520123@yahoo.com.tw>
2019-10-26 22:22:05 +0800

    Add power of three.

--- LeetCodePlayground/PlaygroundPage/Easy/Math/Power of Three Solution.playground/Contents.swift
+++ LeetCodePlayground/PlaygroundPage/Easy/Math/Power of Three Solution.playground/Contents.swift
@@ -1,63 +1,38 @@
-//Write a program that outputs the string representation of numbers from 1 to n.
+//Power of Three
+//Given an integer, write a function to determine if it is a power of three.
 //
-//But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
-
-//n = 15,
+//Example 1:
 //
-//Return:
-//[
-//"1",
-//"2",
-//"Fizz",
-//"4",
-//"Buzz",
-//"Fizz",
-//"7",
-//"8",
-//"Fizz",
-//"Buzz",
-//"11",
-//"Fizz",
-//"13",
-//"14",
-//"FizzBuzz"
-//]
+//Input: 27
+//Output: true
+//Example 2:
+//
+//Input: 0
+//Output: false
+//Example 3:
+//
+//Input: 9
+//Output: true
+//Example 4:
+//
+//Input: 45
+//Output: false
+//Follow up:
+//Could you do it without using any loop / recursion?
 
 class Solution {
-  func fizzBuzz(_ n: Int) -> [String] {
-    var arr = [String]()
-    for i in 0..<n {
-      let t = i + 1
-      if t % 3 == 0 && t % 5 == 0{
-        arr.append("FizzBuzz")
-      } else if t % 3 == 0 {
-        arr.append("Fizz")
-      } else if t % 5 == 0 {
-        arr.append("Buzz")
-      } else {
-        arr.append(String(t))
-      }
+    func isPowerOfThree(_ n: Int) -> Bool {
+        if n < 1 { return false }
+        var tn = n
+        while tn % 3 == 0 {
+            tn /= 3
+        }
+        
+        return tn == 1
     }
-    return arr
-  }
 }
+print(Solution().isPowerOfThree(27) == true)
+print(Solution().isPowerOfThree(0) == false)
+print(Solution().isPowerOfThree(9) == true)
+print(Solution().isPowerOfThree(45) == false)
 
-let ans = [
-  "1",
-  "2",
-  "Fizz",
-  "4",
-  "Buzz",
-  "Fizz",
-  "7",
-  "8",
-  "Fizz",
-  "Buzz",
-  "11",
-  "Fizz",
-  "13",
-  "14",
-  "FizzBuzz"
-]
-
-print(Solution().fizzBuzz(15) == ans)
